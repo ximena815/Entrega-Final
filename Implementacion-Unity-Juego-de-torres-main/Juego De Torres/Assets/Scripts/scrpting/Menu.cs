ea80322abfafdfe5290fdefcd7b4a2b79815d0f3
Ximena <duartealejandra293@gmail.com>
2023-04-03 23:03:58 -0500

    Ajustes y animaciones

---
+++ Implementacion-Unity-Juego-de-torres-main/Juego De Torres/Assets/Scripts/scrpting/Menu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menuPausa;
+
+    [SerializeField]
+    private GameObject botonDePausa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PaNivel1()
+    {
+        SceneManager.LoadScene("Nivel_2");
+    }
+
+    public void Pause()
+    {
+        menuPausa.SetActive(true);
+        botonDePausa.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void Replay()
+    {
+        menuPausa.SetActive(false);
+        botonDePausa.SetActive(true);
+        Time.timeScale = 1f;
+    }
+
+    public void exit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+}
